390ce6fc503db750594a4599a6d8c4fe67321962
Eric Schoffstall <contra@wearefractal.com>
2014-01-13 04:59:00 -0700

    initial implementation

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 var gulp = require('gulp');
 
+var loadTask = function(taskName){
+  var taskConfig = graspfile[taskName];
+  
+  if (!Array.isArray(taskConfig.deps)) taskConfig.deps = [];
+  
+  gulp.task(taskName, taskConfig.deps, function(){
+    var stream = gulp.src(taskConfig.src);
+    // for each plugin pipe stream to it
+    taskConfig.plugins.forEach(function(plugArr){
+      var pluginPath = plugArr.shift();
+      var plugin = require(pluginPath);
+      var inst = plugin.call(plugin, plugArr);
+      stream = stream.pipe(inst);
+    });
+    // pipe it to the output
+    return stream.pipe(taskConfig.dest);
+  });
+  return gulp;
+};
 
-module.exports = gulp;
+module.exports = function(gaspfile) {
+  Object.keys(graspfile).forEach(loadTask);
+  return gulp;
+};
